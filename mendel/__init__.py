d1fd01154547f2489c4a9b5a74899be70838b203
Sumin Byeon <suminb@gmail.com>
2013-05-22 16:13:54 -0700

    Embedding a CSS.

--- mendel/__init__.py
+++ mendel/__init__.py
@@ -1,11 +1,11 @@
-"""Usage: mendel [-h] [--css=CSS]
+"""Usage: mendel [-h] [--css=CSS] [--embed=CSS]
 
 Converts
 
 Options:
   -h --help
   --css=CSS   path of a stylesheet file
-
+  --embed=CSS path of a stylesheet file to be embeded
 """
 
 from docopt import docopt
@@ -24,9 +24,19 @@ def main():
         print '  <link rel="stylesheet" type="text/css" href="%s"/>' % args['--css']
         print '</head>'
         print '<body>'
+
+    # TODO: Revise this
+    if args['--embed'] != None:
+        print '<style type="text/css">'
+        print open(args['--embed'], 'r').read()
+        print '</style>'
     
     print markdown(sys.stdin.read())
 
     if args['--css'] != None:
         print '</body>'
         print '</html>'
+
+if __name__ == '__main__':
+    main()
+
